eddbba634610f01adb5049220c06aa72237a5891
windyfree16 <lwindyfree@gmail.com>
2017-01-03 14:55:00 +0800

    test66

---
+++ method.php
@@ -0,0 +1,30 @@
+<?php
+class Foo {
+	public function PublicMethod(){}
+    private function PrivateMethod(){}
+    public static function PublicStaticMethod(){}
+    private static function PrivateStaticMethod(){}
+}
+
+
+$foo = new Foo();
+
+$callbacks = array(
+		array($foo, 'PublicMethod'),
+		array($foo, 'PrivateMethod'),
+		array($foo, 'PublicStaticMethod'),
+		array($foo, 'PrivateStaticMethod'),
+
+		array('Foo', 'PublicMethod'),
+		array('Foo', 'PrivateMethod'),
+		array('Foo', 'PublicStaticMethod'),
+		array('Foo', 'PrivateStaticMethod'),
+	);
+
+foreach ($callbacks as $callback) {
+	var_dump($callback);
+	var_dump(method_exists($callback[0], $callback[1]));
+	var_dump(is_callable($callback));
+	echo str_repeat('-', 40);
+	echo '<br/>';
+}
